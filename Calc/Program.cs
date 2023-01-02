c73cc556c4b192e9e4f50db6ffd94cfc47e2b5dc
Lexxx <simankovich@inbox.ru>
2023-01-02 21:37:59 +0400

    самый первый релиз простейшего калькулятора

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,6 +1,30 @@
-﻿Console.WriteLine("Введите число: ");
+﻿Console.WriteLine("Простой консольный калькулятор 1.0");
+Console.WriteLine("Введите число: ");
 double num1 = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите действие: ");
+Console.WriteLine("Введите действие(1-*, 2-/, 3--, 4-+, 5-степень): ");
 int sym = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число: ");
 double num2 = int.Parse(Console.ReadLine()!);
+switch (sym)
+{
+    case 1: {
+        Console.WriteLine($"Итого:{num1 * num2}");
+        break;
+    }
+    case 2: {
+        Console.WriteLine($"Итого:{num1 / num2}");
+        break;
+    }
+    case 3: {
+        Console.WriteLine($"Итого:{num1 - num2}");
+        break;
+    }
+    case 4: {
+        Console.WriteLine($"Итого:{num1 + num2}");
+        break;
+    }
+    case 5: {
+        Console.WriteLine($"Итого:{Math.Pow(num1, num2)}");
+        break;
+    }
+}
