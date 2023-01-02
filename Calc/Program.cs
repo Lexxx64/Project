c0fb73de88ad042976e231f31e3f350405a08509
Lexxx <simankovich@inbox.ru>
2023-01-02 17:42:53 +0400

    create new project Calc

---
+++ Calc/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+double num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите действие: ");
+int sym = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число: ");
+double num2 = int.Parse(Console.ReadLine()!);
