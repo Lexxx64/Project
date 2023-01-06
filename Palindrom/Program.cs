2ade74f73d15d487d7f2478175ce7ae747e14978
Lexxx <simankovich@inbox.ru>
2023-01-06 11:54:55 +0400

    create console palindrom

---
+++ Palindrom/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Проверка на палиндром 1.0");
+Console.WriteLine("Введите данные для проверки: ");
+string text = Console.ReadLine()!;
+text = text.Replace(" ", "");
+char[] obrtext = text.ToCharArray();
+Array.Reverse(obrtext);
+string revertext = new string(obrtext);
+if (text == revertext){
+    Console.Write($"Является палиндромом!");
+}
+else{
+    Console.Write($"Не является палиндромом!");
+}
+
